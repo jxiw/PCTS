033d596a191f1f2285e9572eaf1d76ca263cfb5e
Junxiong Wang <chuangzhetianxia@gmail.com>
2022-02-08 15:58:50 -0500

    remove cache

--- experiments/run_pcts.sh
+++ experiments/run_pcts.sh
@@ -18,6 +18,7 @@ mkdir "HOO/CurrinExp/"
 mkdir "HOO/Branin/"
 mkdir "HOO/Borehole/"
 
+# ucbv
 for i in `seq $start $round`;
 do
   # 0.02
@@ -38,49 +39,16 @@ do
 done
 
 # ucb1
-#for i in `seq $start $round`;
-#do
-#  python -u run_dnf_synthetic.py UCB1 Hartmann3 HOO 3 > "HOO/Hartmann3/HOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Hartmann3 DHOO 3 > "DHOO/Hartmann3/DHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Hartmann6 HOO 3 > "HOO/Hartmann6/HOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Hartmann6 DHOO 3 > "DHOO/Hartmann6/DHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 CurrinExp HOO 3 > "HOO/CurrinExp/HOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 CurrinExp DHOO 3 > "DHOO/CurrinExp/DHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Branin HOO 3 > "HOO/Branin/HOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Branin DHOO 3 > "DHOO/Branin/DHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Borehole HOO 3 > "HOO/Borehole/HOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Borehole DHOO 3 > "DHOO/Borehole/DHOO_UCB1_$i.txt"
-#done
-
-#for i in `seq $start $round`;
-#do
-#  for j in 4 5 6 7
-#    do
-#      #  python -u run_dnf_synthetic.py UCB1 Hartmann3 HOO 1 > "HOO/Hartmann3/1HOO_UCB1_$i.txt"
-#      #  python -u run_dnf_synthetic.py UCB1 Hartmann3 DHOO 1 > "DHOO/Hartmann3/1DHOO_UCB1_$i.txt"
-#      #  python -u run_dnf_synthetic.py UCB1 Hartmann6 HOO 1 > "HOO/Hartmann6/1HOO_UCB1_$i.txt"
-#      #  python -u run_dnf_synthetic.py UCB1 Hartmann6 DHOO 1 > "DHOO/Hartmann6/1DHOO_UCB1_$i.txt"
-#      #  python -u run_dnf_synthetic.py UCB1 CurrinExp HOO 1 > "HOO/CurrinExp/1HOO_UCB1_$i.txt"
-#      #  python -u run_dnf_synthetic.py UCB1 CurrinExp DHOO 1 > "DHOO/CurrinExp/1DHOO_UCB1_$i.txt"
-#      #  python -u run_dnf_synthetic.py UCB1 Branin HOO ${j} > "HOO/Branin/${j}HOO_UCB1_$i.txt"
-#      #  python -u run_dnf_synthetic.py UCB1 Branin DHOO ${j} > "DHOO/Branin/${j}DHOO_UCB1_$i.txt"
-#      #  python -u run_dnf_synthetic.py UCB1 Branin HOO 1 > "HOO/Branin/1HOO_UCB1_$i.txt"
-#      #  python -u run_dnf_synthetic.py UCB1 Branin DHOO 1 > "DHOO/Branin/1DHOO_UCB1_$i.txt"
-#      #  python -u run_dnf_synthetic.py UCB1 Borehole HOO 1 > "HOO/Borehole/1HOO_UCB1_$i.txt"
-#      #  python -u run_dnf_synthetic.py UCB1 Borehole DHOO 1 > "DHOO/Borehole/1DHOO_UCB1_$i.txt"
-#    done
-#done
-
-#for i in `seq $start $round`;
-#do
-#  python -u run_dnf_synthetic.py UCB1 Branin HOO -1 > "HOO/Branin/MHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Branin DHOO -1 > "DHOO/Branin/MDHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Hartmann3 HOO -1 > "HOO/Hartmann3/MHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Hartmann3 DHOO -1 > "DHOO/Hartmann3/MDHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Hartmann6 HOO -1 > "HOO/Hartmann6/MHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Hartmann6 DHOO -1 > "DHOO/Hartmann6/MDHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 CurrinExp HOO -1 > "HOO/CurrinExp/MHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 CurrinExp DHOO -1 > "DHOO/CurrinExp/MDHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Borehole HOO -1 > "HOO/Borehole/MHOO_UCB1_$i.txt"
-#  python -u run_dnf_synthetic.py UCB1 Borehole DHOO -1 > "DHOO/Borehole/MDHOO_UCB1_$i.txt"
-#done
+for i in `seq $start $round`;
+do
+  python -u run_dnf_synthetic.py UCB1 Hartmann3 HOO 3 > "HOO/Hartmann3/HOO_UCB1_$i.txt"
+  python -u run_dnf_synthetic.py UCB1 Hartmann3 DHOO 3 > "DHOO/Hartmann3/DHOO_UCB1_$i.txt"
+  python -u run_dnf_synthetic.py UCB1 Hartmann6 HOO 3 > "HOO/Hartmann6/HOO_UCB1_$i.txt"
+  python -u run_dnf_synthetic.py UCB1 Hartmann6 DHOO 3 > "DHOO/Hartmann6/DHOO_UCB1_$i.txt"
+  python -u run_dnf_synthetic.py UCB1 CurrinExp HOO 3 > "HOO/CurrinExp/HOO_UCB1_$i.txt"
+  python -u run_dnf_synthetic.py UCB1 CurrinExp DHOO 3 > "DHOO/CurrinExp/DHOO_UCB1_$i.txt"
+  python -u run_dnf_synthetic.py UCB1 Branin HOO 3 > "HOO/Branin/HOO_UCB1_$i.txt"
+  python -u run_dnf_synthetic.py UCB1 Branin DHOO 3 > "DHOO/Branin/DHOO_UCB1_$i.txt"
+  python -u run_dnf_synthetic.py UCB1 Borehole HOO 3 > "HOO/Borehole/HOO_UCB1_$i.txt"
+  python -u run_dnf_synthetic.py UCB1 Borehole DHOO 3 > "DHOO/Borehole/DHOO_UCB1_$i.txt"
+done
